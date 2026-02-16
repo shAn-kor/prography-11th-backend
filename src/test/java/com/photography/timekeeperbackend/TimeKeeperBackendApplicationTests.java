115899365b6ac0477fbfeee3e34c2af593e3bc8a
shAn-kor <kjn0406@naver.com>
2026-02-16 15:08:37 +0900

    project init

---
+++ src/test/java/com/photography/timekeeperbackend/TimeKeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photography.timekeeperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeKeeperBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
